da235a071c04f0a13fbd8742d7b3352e297fa4c6
ehmicky <ehmicky@gmail.com>
2019-03-30 10:00:00 +0100

    Fix code duplication

--- test/helpers/testing/jest.test.js
+++ test/helpers/testing/jest.test.js
@@ -8,7 +8,7 @@ const { callMain } = require('./main')
 const name = callMain()
 
 // eslint-disable-next-line no-undef
-describe('should make tests fail', () => {
+describe('should make test fail', () => {
   // eslint-disable-next-line no-undef
   test(`on ${name}`, () => {
     // eslint-disable-next-line no-empty-function, max-nested-callbacks
